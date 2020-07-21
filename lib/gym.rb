b79ffe346a1597a34b86ac63351c61b6317411ce
allenorton <norton.allene@gmail.com>
2020-07-21 08:18:32 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,29 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
+  def memberships
+    Membership.all.select {|mship| mship.gym == self}
+  end
+
+  def lifters
+    memberships.map {|mship| mship.lifter}
+  end
+
+  def lifter_names
+    lifters.map {|lftr| lftr.name}
+  end
+
+  def combined_lift
+    lifters.sum {|lftr| lftr.lift_total}
   end
 end
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,15 @@
 class Membership
-  attr_reader :cost
+  attr_reader :gym, :cost, :lifter
+  @@all = []
 
-  def initialize(cost)
+  def initialize(gym, cost, lifter)
+    @gym = gym
     @cost = cost
+    @lifter = lifter
+    self.class.all << self 
+  end
+
+  def self.all
+    @@all
   end
 end
